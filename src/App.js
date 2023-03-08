e80f62b90c4cd73d5baf4586fbbbf613c459da86
J34993R <guialves.oliveira1234@gmail.com>
2023-03-08 12:02:54 -0300

    Progresso até a aula 18

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
 
 class App extends Component {
     state = {
-        name: 'Guilherme',
-        counter: 0
+        counter: 0,
+        posts: [
+            {
+                id: 1,
+                title: 'O titulo 1',
+                body: 'O corpo 1'
+            },
+            {
+                id: 2,
+                title: 'O titulo 2',
+                body: 'O corpo 2'
+            },
+            {
+                id: 3,
+                title: 'O titulo 3',
+                body: 'O corpo 3'
+            }
+        ]
     };
 
-
-    handlePClick = () => {
-        this.setState({name: 'Alves'})
+    componentDidMount() {
+        this.handleTimeOut()
     }
 
-    handleAClick = (event) => {
-        event.preventDefault()
-        const {counter} = this.state;
-        this.setState({counter: counter + 1})
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.handleTimeOut();
+    }
+
+    handleTimeOut() {
+        const {posts, counter} = this.state;
+        posts[0].title = 'O titulo 1 Mudou';
+
+        setTimeout(() => {
+            this.setState({posts, counter: counter + 1})
+        }, 50000)
     }
 
     render() {
-        const {name, counter} = this.state;
+        const {posts, counter} = this.state;
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p onClick={this.handlePClick}>
-                        {name} {counter}
-                    </p>
-                    <a onClick={this.handleAClick}
-                       className="App-link"
-                       href="https://reactjs.org"
-                       target="_blank"
-                       rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <h1>{counter}</h1>
+                {posts.map(post => (
+                        <div key={post.id}>
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                        </div>
+                    )
+                )}
             </div>
         );
     }
